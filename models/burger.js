9e7baa73a2a1822dfedb2fe37915f4012a8900f2
Kate <kate.stark13@sbcglobal.net>
2018-02-21 22:16:53 -0600

    update burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,4 +2,27 @@
 var orm = require("../config/orm.js");
 
 //call the ORM functions using burger specific input for the ORM
-var 
+var burger = {
+	selectAll: function(cb) {
+		orm.selectAll("burgers", function(res){
+			cb(res);
+		});
+	},
+	insertOne: function(cols, vals, cb) {
+		orm.insertOne("burgers", cols, vals, function(res){
+			cb(res);
+		});
+	},
+
+	updateOne: function(objColVals, condition, cb) {
+		orm.updateOne("burgers", objColVals, condition, function(res){
+			cb(res);
+		});
+	}
+
+};
+
+
+
+//export the database functions for the controller
+module.exports = burger;
