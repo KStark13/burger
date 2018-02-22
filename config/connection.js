236162552bc3fd95fe7790b42ecd5309c1b13106
Kate <kate.stark13@sbcglobal.net>
2018-02-21 20:48:55 -0600

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,24 @@
-//set up code to connect node to mysql
-//export the connection
+//dependencies
+var mysql = require('mysql');
+
+//create mysql connection object
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "burgers_db"
+});
+
+connection.connect(function(err){
+	if(err) {
+		console.log("error connecting: " + err.stack);
+		return;
+	}
+	console.log("Connected as id " + connection.threadId);
+});
+
+
+
+
+//export connection for ORM use
+module.exports = connection;
